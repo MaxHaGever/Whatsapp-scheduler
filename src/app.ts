2be1ea7d23e8fdb68569f10018e9e89b37713c39
MaxHaGever <maxspectorr@gmail.com>
2026-01-27 22:47:39 +0200

    Mount auth routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import whatsappRoutes from "./routes/whatsappRoutes";
 import googleRoutes from "./routes/googleRoutes";
 import debugeRoutes from "./routes/debugRoutes";
+import authRoutes from "./routes/authRoutes";
 
 export function createApp() {
   const app = express();
@@ -9,6 +10,7 @@ export function createApp() {
 
   app.get("/health", (_req, res) => res.send("ok"));
 
+  app.use("/api", authRoutes);
   app.use("/webhook/whatsapp", whatsappRoutes);
   app.use(googleRoutes);
   app.use("/debug", debugeRoutes);
