a18f32ccb8b855b5d8d28ab4722793c5d2d22b22
MaxHaGever <maxspectorr@gmail.com>
2026-01-27 23:28:37 +0200

    Remove legacy Google callback route

--- src/routes/googleRoutes.ts
+++ src/routes/googleRoutes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { startGoogleAuth, handleGoogleOAuthCallback, googleStatus } from "../controller/googleController";
+import { googleStatus } from "../controller/googleController";
 
 const router = Router();
 
-router.get("/auth/google", startGoogleAuth);
-router.get("/oauth2callback", handleGoogleOAuthCallback);
 router.get("/api/google/status", googleStatus);
 
 export default router;
